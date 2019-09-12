cfaebe85dda6a440fe12c93e7b9c2dcb31fbcdb4
Jake Jarvis <jake@jarv.is>
2019-09-12 12:51:39 -0400

    Check for required environment variables

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,6 +2,26 @@
 
 set -e
 
+if [ -z "$B2_BUCKET" ]; then
+  echo "B2_BUCKET is not set. Quitting."
+  exit 1
+fi
+
+if [ -z "$B2_APPKEY_ID" ]; then
+  echo "B2_APPKEY_ID is not set. Quitting."
+  exit 1
+fi
+
+if [ -z "$B2_APPKEY" ]; then
+  echo "B2_APPKEY is not set. Quitting."
+  exit 1
+fi
+
+if [ -z "$SOURCE_DIR" ]; then
+  echo "SOURCE_DIR is not set. Quitting."
+  exit 1
+fi
+
 b2 authorize-account ${B2_APPKEY_ID} ${B2_APPKEY}
 
 b2 sync --delete --replaceNewer ${SOURCE_DIR} ${B2_BUCKET}
